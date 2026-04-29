87a6b829df3b4d5902e4ae8681a549ade113e378
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-29 15:22:34 +0530

    chatPage: if not verified route back to login page

--- src/chat/ChatPage.tsx
+++ src/chat/ChatPage.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { UseAppData } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
+import Loading from "../components/Loading";
+
 const ChatPage = () => {
+  const navigate = useNavigate();
+  const { loading, isAuth } = UseAppData();
+  useEffect(() => {
+    if (!isAuth && !loading) {
+      navigate("/login");
+    }
+  }, [loading, isAuth, navigate]);
+
+  if (loading) return <Loading />;
   return (
     <>
       <h2>Chat</h2>
