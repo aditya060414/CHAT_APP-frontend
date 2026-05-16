66eaab975cc5fd77ab4ed4554f5c83bc18e60d51
aditya060414 <singh.aditya.44618@gmail.com>
2026-05-16 18:37:33 +0530

    main: pass socketContext to children

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AppProvider } from "./context/AppContext.tsx";
+import { SocketProvider } from "./context/SocketContext.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <SocketProvider><App /></SocketProvider>
     </AppProvider>
   </StrictMode>,
 );
